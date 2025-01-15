8176dcde6cc0922a2499923c956de8db63f1c68d
Leon Fehr <leon.fehr@student.htldornbirn.at>
2025-01-15 10:39:26 +0100

    orf weiter gemacht javascript angefangen mit klasse

---
+++ CSS/JS/index.js
@@ -0,0 +1,9 @@
+let s = 100;
+
+console.log("Typ der variable c ist: " + typeof c);
+console.log("Wert von der variable a ist: " + c);
+
+s = "2" + 2;
+
+console.log("Der Typ von s ist: " + typeof s);
+console.log("Der Wert von s ist: " + s);
