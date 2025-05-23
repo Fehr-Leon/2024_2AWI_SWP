33b9d3a10569eb1bb641637db812277d9b92f9e2
Leon Fehr <leon.fehr@student.htldornbirn.at>
2025-05-23 13:56:11 +0200

    Zugriff Html&js

--- Exercises/Html&JS/Aufgabe1/index.js
+++ Exercises/Html&JS/Aufgabe1/index.js
@@ -1,3 +1,67 @@
-let let element = document.getElementById('content');
-element.innerHTML = 'Tolle Sache';
-console.log(element);
+function calculateSum() {
+    console.log("Calculete Clicked");
+
+    inputA = document.getElementById("inputA").value;
+    inputB = document.getElementById("inputB").value;
+
+    console.log("inputA: " + inputA);
+    console.log("inputB: " + inputB);
+
+    let result = Number(inputA) + Number(inputB);
+
+    console.log(result);
+
+    let resultElement = document.getElementById("result")
+    resultElement.innerHTML = result;
+}
+
+function calculateSub() {
+    console.log("Calculete Clicked");
+
+    inputA = document.getElementById("inputA").value;
+    inputB = document.getElementById("inputB").value;
+
+    console.log("inputA: " + inputA);
+    console.log("inputB: " + inputB);
+
+    let result = Number(inputA) - Number(inputB);
+
+    console.log(result);
+
+    let resultElement = document.getElementById("result")
+    resultElement.innerHTML = result;
+}
+function calculateMul() {
+    console.log("Calculete Clicked");
+
+    inputA = document.getElementById("inputA").value;
+    inputB = document.getElementById("inputB").value;
+
+    console.log("inputA: " + inputA);
+    console.log("inputB: " + inputB);
+
+    let result = Number(inputA) * Number(inputB);
+
+    console.log(result);
+
+    let resultElement = document.getElementById("result")
+    resultElement.innerHTML = result;
+}
+
+
+function calculateDiv() {
+    console.log("Calculete Clicked");
+
+    inputA = document.getElementById("inputA").value;
+    inputB = document.getElementById("inputB").value;
+
+    console.log("inputA: " + inputA);
+    console.log("inputB: " + inputB);
+
+    let result = Number(inputA) / Number(inputB);
+
+    console.log(result);
+
+    let resultElement = document.getElementById("result")
+    resultElement.innerHTML = result;
+}
