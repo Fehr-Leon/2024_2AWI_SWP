40f646d0ed23c35d0b198a2a345ec7e0eb2a6e75
Leon Fehr <leon.fehr@student.htldornbirn.at>
2025-06-06 14:21:57 +0200

    Zufallsgenerator abgabe

--- Exercises/Html&JS/Aufgabe2/index.js
+++ Exercises/Html&JS/Aufgabe2/index.js
@@ -1,11 +1,12 @@
 function generate() {
-    let number1 = praseInt(document.getElementById("InputA").value);
-    let number2 = parseInt(document.getElementById("InputB").value);
+    let number1 = document.getElementById("InputA").value;
+    let number2 = document.getElementById("InputB").value;
 
     let differenz = parseInt(number2) - parseInt(number1);
 
 
-    let randomNumber = Math.floor(Math.random() * differenz) + parseInt(number1);
+    let randomNumber = Math.floor(Math.random() * differenz) + parseInt(number1) + 1;
+    console.log(randomNumber);
 
     document.getElementById("randomNumber").innerHTML = randomNumber;
 
