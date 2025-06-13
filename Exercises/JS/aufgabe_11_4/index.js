a67ac743312f48e0c48cfb2be43e34c1ffb5e3ef
Leon Fehr <leon.fehr@student.htldornbirn.at>
2025-06-13 14:45:44 +0200

    aufgabe 4 arrays fertig

---
+++ Exercises/JS/aufgabe_11_4/index.js
@@ -0,0 +1,18 @@
+let p = 20;
+let h = 100;
+let r = 600;
+
+let geld = p * h;
+
+if (geld < r) {
+    console.log("NO");
+}
+
+else if (geld === r) {
+    console.log("BARELY");
+}
+
+else if (geld > r) {
+    console.log("YES");
+
+}
