a9d2251418600d7b4294f4a89a7820dda3721273
Leon Fehr <leon.fehr@student.htldornbirn.at>
2025-06-13 14:37:41 +0200

    Aufgabe 3 arrays fertig

--- Exercises/Html&JS/Aufgabe1/index.js
+++ Exercises/Html&JS/Aufgabe1/index.js
@@ -4,12 +4,9 @@ function calculateSum() {
     inputA = document.getElementById("inputA").value;
     inputB = document.getElementById("inputB").value;
 
-    console.log("inputA: " + inputA);
-    console.log("inputB: " + inputB);
 
     let result = Number(inputA) + Number(inputB);
 
-    console.log(result);
 
     let resultElement = document.getElementById("result")
     resultElement.innerHTML = result;
@@ -21,12 +18,8 @@ function calculateSub() {
     inputA = document.getElementById("inputA").value;
     inputB = document.getElementById("inputB").value;
 
-    console.log("inputA: " + inputA);
-    console.log("inputB: " + inputB);
-
     let result = Number(inputA) - Number(inputB);
 
-    console.log(result);
 
     let resultElement = document.getElementById("result")
     resultElement.innerHTML = result;
@@ -37,12 +30,9 @@ function calculateMul() {
     inputA = document.getElementById("inputA").value;
     inputB = document.getElementById("inputB").value;
 
-    console.log("inputA: " + inputA);
-    console.log("inputB: " + inputB);
 
     let result = Number(inputA) * Number(inputB);
 
-    console.log(result);
 
     let resultElement = document.getElementById("result")
     resultElement.innerHTML = result;
@@ -55,12 +45,9 @@ function calculateDiv() {
     inputA = document.getElementById("inputA").value;
     inputB = document.getElementById("inputB").value;
 
-    console.log("inputA: " + inputA);
-    console.log("inputB: " + inputB);
 
     let result = Number(inputA) / Number(inputB);
 
-    console.log(result);
 
     let resultElement = document.getElementById("result")
     resultElement.innerHTML = result;
--- Exercises/JS/aufgabe_11_3/index.js
+++ Exercises/JS/aufgabe_11_3/index.js
@@ -1,8 +1,35 @@
-let zahleninput = [1, -2, -8, 4, 5];
+//Pius hat bisschen geholfen
 
+let input = "5 1 -2 -8 4 -5";
+let zahlen = input.split(" ").map(Number);
+let Betragzahlen = zahlen.map(Betrag);
 
-for (let i = 0; i <= zahleninput.length; i++) {
-    if (zahleninput[i] == ) {
-
+function Betrag(zahl) {
+    if (zahl < 0) {
+        zahl * -1;
+        return zahl;
+    } else {
+        return zahl;
     }
 }
+
+function nahnull(zahlen, Betragzahlen) {
+    let nahezahl = zahlen[0];
+    let nahebetrag = Betragzahlen[0];
+
+    for (let index = 0; index < zahlen.length; index++) {
+        if (Betragzahlen[index] < nahebetrag) {
+            nahebetrag = Betragzahlen[index];
+            nahezahl = zahlen[index];
+        } else if (nahebetrag === Betragzahlen[index]) {
+            if (zahlen[index] > nahezahl) {
+                nahezahl = zahlen[index];
+            }
+        }
+    }
+
+    console.log(nahezahl);
+    return nahezahl;
+}
+
+nahnull(zahlen, Betragzahlen);
